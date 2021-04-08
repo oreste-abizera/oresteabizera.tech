3f42d8a69fa84d4342fce53517dcbc99bf982d41
oreste-abizera <oresteabizera11@gmail.com>
2021-04-08 20:20:09 +0200

    Finishing about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,7 +12,17 @@ export default class AboutMe extends Component {
           <div>
             <img src={img} className={styles.aboutImage} alt="Profile"></img>
           </div>
-          <div></div>
+          <div className={styles.discover}>
+            <h2>DISCOVER MY INFO</h2>
+            <p>
+              I am a Software Engineer based in Rwanda. I focus on writing some
+              codes everyday to solve many problems. I use <b>React js</b> ,{" "}
+              <b>Next js</b> and <b>Gatsby</b> for frontend on the web.{" "}
+              <b>Node js</b> helps me on backend and <b>React Native</b> for
+              mobile development.
+            </p>
+            <i>"Talk is cheap, write some code."</i>
+          </div>
         </div>
       </div>
     )
