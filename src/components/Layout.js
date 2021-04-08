4ae9fbb5726c46ba81e38fd0070a4f500263a2e2
oreste-abizera <oresteabizera11@gmail.com>
2021-04-08 11:20:57 +0200

    Alignment of Navbar, Footer and Content

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react"
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+import "../css/layout.css"
 
 export default class Layout extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
-        <main>{this.props.children}</main>
+        <main className="main">{this.props.children}</main>
         <Footer />
       </React.Fragment>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,6 @@ import React, { Component } from "react"
 
 export default class Navbar extends Component {
   render() {
-    return <div>Navbar</div>
+    return <nav className="navbar">Navbar</nav>
   }
 }
