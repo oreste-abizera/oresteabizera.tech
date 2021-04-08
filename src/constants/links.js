b80a84876b9324652edbcfb16dadbbce5eff204f
oreste-abizera <oresteabizera11@gmail.com>
2021-04-08 12:13:17 +0200

    Links constant

---
+++ src/constants/links.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    path: "/#home",
+    text: "home",
+  },
+  {
+    path: "/#about",
+    text: "about me",
+  },
+  {
+    path: "/#services",
+    text: "services",
+  },
+  {
+    path: "/#projects",
+    text: "projects",
+  },
+  {
+    path: "/#contact",
+    text: "contact",
+  },
+]
