2ad39149b2c22d711f7a6d27cbde37bee458a2fe
oreste-abizera <oresteabizera11@gmail.com>
2021-04-08 13:50:45 +0200

    Finishing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,31 @@
 import React, { Component } from "react"
-
+import * as styles from "../css/footer.module.css"
+import links from "../constants/links"
+import socialIcons from "../constants/social-icons"
+import { Link } from "gatsby"
 export default class Footer extends Component {
   render() {
-    return <footer className="footer">Footer</footer>
+    return (
+      <footer className={`footer ${styles.footer}`}>
+        <div className={styles.footerLinks}>
+          {links.map((link, index) => (
+            <Link key={index} to={link.path}>
+              {link.text}
+            </Link>
+          ))}
+        </div>
+        <div className={styles.socialIcons}>
+          {socialIcons.map((icon, index) => (
+            <Link key={index} to={icon.url} target="_blank">
+              {icon.icon}
+            </Link>
+          ))}
+        </div>
+        <div className={styles.copyright}>
+          Copyright &copy; Abizera Oreste {new Date().getFullYear()} All rights
+          reserved
+        </div>
+      </footer>
+    )
   }
 }
