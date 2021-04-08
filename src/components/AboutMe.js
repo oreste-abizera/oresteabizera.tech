8ab75d71d1ffcb02383d3c66f0e510ce99bc9175
oreste-abizera <oresteabizera11@gmail.com>
2021-04-08 19:28:12 +0200

    About me image

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react"
 import * as styles from "../css/about.module.css"
 import Title from "./Title"
+import img from "../images/bg1.jpg"
 
 export default class AboutMe extends Component {
   render() {
     return (
       <div id="about" className={styles.about}>
         <Title first="About" last="Me"></Title>
+        <div className={styles.info}>
+          <div>
+            <img src={img} className={styles.aboutImage}></img>
+          </div>
+          <div></div>
+        </div>
       </div>
     )
   }
