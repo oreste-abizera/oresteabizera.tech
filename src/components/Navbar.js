adb219f5b5ad7b35808aeae984a39a87fb1f1806
oreste-abizera <oresteabizera11@gmail.com>
2021-04-08 11:45:41 +0200

    Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,22 @@
+import { Link } from "gatsby"
 import React, { Component } from "react"
+import * as styles from "../css/navbar.module.css"
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isOpen: true,
+    }
+  }
   render() {
-    return <nav className="navbar">Navbar</nav>
+    return (
+      <nav className={`navbar ${styles.navbar}`}>
+        <Link to="/#home" className={styles.logo}>
+          <span>A</span>
+          <span>O</span>
+        </Link>
+      </nav>
+    )
   }
 }
