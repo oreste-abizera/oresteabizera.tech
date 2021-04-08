80555f4930a9a6236fb08a0d941a9ec9f4d44b63
oreste-abizera <oresteabizera11@gmail.com>
2021-04-08 16:26:56 +0200

    Real social icon urls

--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -9,23 +9,23 @@ import {
 const icons = [
   {
     icon: <FaDev />,
-    url: "https://twitter.com/?lang=en",
+    url: "https://dev.to/oreste",
   },
   {
     icon: <FaTwitterSquare />,
-    url: "https://twitter.com/?lang=en",
+    url: "https://twitter.com/abizeraoreste",
   },
   {
     icon: <FaInstagramSquare />,
-    url: "https://twitter.com/?lang=en",
+    url: "https://www.instagram.com/coder__tauras/",
   },
   {
     icon: <FaLinkedin />,
-    url: "https://twitter.com/?lang=en",
+    url: "https://www.linkedin.com/in/oreste-abizera-151bb9194",
   },
   {
     icon: <FaGithubSquare />,
-    url: "https://twitter.com/?lang=en",
+    url: "https://github.com/oreste-abizera",
   },
 ]
 
