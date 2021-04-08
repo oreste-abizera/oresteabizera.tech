6e7e76d52354d27b584062d7f956715267a2bbcf
oreste-abizera <oresteabizera11@gmail.com>
2021-04-08 12:11:21 +0200

    Navbar right icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,14 @@ import { Link } from "gatsby"
 import React, { Component } from "react"
 import * as styles from "../css/navbar.module.css"
 import { FaAlignRight } from "react-icons/fa"
+import socialIcons from "../constants/social-icons"
 
 export default class Navbar extends Component {
   constructor(props) {
     super(props)
     this.state = {
       isOpen: true,
+      socialIcons,
     }
   }
   render() {
@@ -17,8 +19,15 @@ export default class Navbar extends Component {
           <span>A</span>
           <span>O</span>
         </Link>
-        <div className={styles.rightIcons}>
-          <FaAlignRight></FaAlignRight>
+        <div>
+          <FaAlignRight className={styles.navToggler}></FaAlignRight>
+          <div className={styles.socialIcons}>
+            {this.state.socialIcons.map((icon, index) => (
+              <Link key={index} to={icon.url} target="_blank">
+                {icon.icon}
+              </Link>
+            ))}
+          </div>
         </div>
       </nav>
     )
---
+++ src/constants/social-icons.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { FaDev, FaTwitterSquare, FaInstagram, FaLinkedin } from "react-icons/fa"
+const icons = [
+  {
+    icon: <FaDev />,
+    url: "https://twitter.com/?lang=en",
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: "https://twitter.com/?lang=en",
+  },
+  {
+    icon: <FaInstagram />,
+    url: "https://twitter.com/?lang=en",
+  },
+  {
+    icon: <FaLinkedin />,
+    url: "https://twitter.com/?lang=en",
+  },
+]
+
+export default icons
