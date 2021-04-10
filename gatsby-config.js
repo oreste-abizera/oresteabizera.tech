0f81c88175046a5535187236943620b2132cbb4b
oreste-abizera <oresteabizera11@gmail.com>
2021-04-10 07:43:39 +0200

    Adding site metadata in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,12 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Oreste Abizera",
+    description:
+      "I am a Software Engineer based in Rwanda. I focus on writing some codes everyday to solve many problems. I use React js, Next js and Gatsby for frontend on the web. Node js helps me on backend and React Native for mobile development.",
+    author: "@abizeraoreste",
+    keywords: ["Gatsby"],
+  },
   plugins: [],
 }
