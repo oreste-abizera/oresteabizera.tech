71d5c67413de128c9a88e7b8f96327817e7db6be
oreste-abizera <oresteabizera11@gmail.com>
2021-04-11 15:33:33 +0200

    Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,20 +1,32 @@
+import { Link } from "gatsby"
 import React, { Component } from "react"
+import { FaEye } from "react-icons/fa"
 import projects from "../constants/projects"
 import * as styles from "../css/projects.module.css"
 import Title from "./Title"
 
 export default class Projects extends Component {
   render() {
-    console.log(projects)
     return (
-      <>
+      <div className={styles.projectsContainer} id="projects">
         <Title first="Featured" last="Works"></Title>
         <div className={styles.projects}>
           {projects.map((project, index) => (
-            <div key={index}>{project.title}</div>
+            <div key={index} className={styles.project}>
+              <img src={project.social_image} alt={project.title}></img>
+              <div className={styles.info}>
+                <div>
+                  <h4>{project.title}</h4>
+                  <p>Type: {project.type}</p>
+                </div>
+                <Link to={`/projects/${project.slug}`}>
+                  <FaEye></FaEye>
+                </Link>
+              </div>
+            </div>
           ))}
         </div>
-      </>
+      </div>
     )
   }
 }
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -5,6 +5,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5fa2dfe2054a04254b4d811a/screenshot.png",
     slug: "whatsapp-clone",
+    type: "Web",
   },
   {
     title: "Redux Cart",
@@ -12,6 +13,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5fa56245f1c7d4012c8f6aea/screenshot.png",
     slug: "redux-cart",
+    type: "Web",
   },
   {
     title: "Upcrew School",
@@ -19,6 +21,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5fb97da139e1490007640e5a/screenshot.png",
     slug: "upcrew",
+    type: "Web",
   },
   {
     title: "Con Fusion",
@@ -26,6 +29,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5fe86839484016000855968d/screenshot.png",
     slug: "con-fusion",
+    type: "Web",
   },
   {
     title: "Markdown Preview",
@@ -33,6 +37,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5feb25f660a1040eb92b609c/screenshot.png",
     slug: "markdown-preview",
+    type: "Web",
   },
 ]
 
