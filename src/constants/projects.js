88fd31645151bf9e0f012c6d531a623d8cf00156
oreste-abizera <oresteabizera11@gmail.com>
2021-04-11 11:45:50 +0200

    Projects constant

---
+++ src/constants/projects.js
@@ -0,0 +1,38 @@
+const projects = [
+  {
+    title: "Whatsapp Clone",
+    url: "https://ao-whatsapp-clone.netlify.app/",
+    social_image:
+      "https://d33wubrfki0l68.cloudfront.net/5fa2dfe2054a04254b4d811a/screenshot.png",
+    slug: "whatsapp-clone",
+  },
+  {
+    title: "Redux Cart",
+    url: "https://ao-redux-cart.netlify.app/",
+    social_image:
+      "https://d33wubrfki0l68.cloudfront.net/5fa56245f1c7d4012c8f6aea/screenshot.png",
+    slug: "redux-cart",
+  },
+  {
+    title: "Upcrew School",
+    url: "https://upcrew.netlify.app/",
+    social_image:
+      "https://d33wubrfki0l68.cloudfront.net/5fb97da139e1490007640e5a/screenshot.png",
+    slug: "upcrew",
+  },
+  {
+    title: "Con Fusion",
+    url: "https://ao-confusion.netlify.app/",
+    social_image:
+      "https://d33wubrfki0l68.cloudfront.net/5fe86839484016000855968d/screenshot.png",
+    slug: "con-fusion",
+  },
+  {
+    id: 7,
+    title: "Markdown Preview",
+    url: "https://react-markdownpreview.netlify.app/",
+    social_image:
+      "https://d33wubrfki0l68.cloudfront.net/5feb25f660a1040eb92b609c/screenshot.png",
+    slug: "markdown-preview",
+  },
+]
