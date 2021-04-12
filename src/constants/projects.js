d9cc0a19d30f45cf70ec30698f55450d44954ac4
oreste-abizera <oresteabizera11@gmail.com>
2021-04-12 12:49:48 +0200

    Fixed navbar on large screens commented for future usage

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -7,7 +7,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5fa2dfe2054a04254b4d811a/screenshot.png",
     slug: "whatsapp-clone",
-    type: "Web",
+    type: "Web application",
     images: [],
     technologies: ["React js", "Firebase", "Material ui"],
     github_url: "https://github.com/oreste-abizera/whatsapp-clone",
@@ -21,7 +21,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5fa56245f1c7d4012c8f6aea/screenshot.png",
     slug: "redux-cart",
-    type: "Web",
+    type: "Web application",
     images: [],
     technologies: ["React js", "Redux"],
   },
@@ -33,7 +33,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5fb97da139e1490007640e5a/screenshot.png",
     slug: "upcrew",
-    type: "Web",
+    type: "Web application",
     images: [],
     technologies: [
       "React js",
@@ -53,7 +53,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5fe86839484016000855968d/screenshot.png",
     slug: "con-fusion",
-    type: "Web",
+    type: "Web application",
     images: [],
     technologies: ["React js"],
     github_url: "https://github.com/oreste-abizera/confusion",
@@ -66,7 +66,7 @@ const projects = [
     social_image:
       "https://d33wubrfki0l68.cloudfront.net/5feb25f660a1040eb92b609c/screenshot.png",
     slug: "markdown-preview",
-    type: "Web",
+    type: "Web application",
     images: [],
     technologies: ["React js"],
   },
