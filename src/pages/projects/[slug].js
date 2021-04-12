7abfe575ceacc9cea1e99a1f42d82e95d380feff
oreste-abizera <oresteabizera11@gmail.com>
2021-04-12 10:45:22 +0200

    Image Modal on single project page

--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -1,6 +1,6 @@
 import { Link } from "gatsby"
 import React, { Component } from "react"
-import { FaEye } from "react-icons/fa"
+import { FaEye, FaTimes } from "react-icons/fa"
 import Banner from "../../components/Banner"
 import Layout from "../../components/Layout"
 import projects from "../../constants/projects"
@@ -27,55 +27,67 @@ export default class SingleProject extends Component {
       })
     }
     return (
-      <Layout>
-        {!currentProject ? (
-          <>
-            <header className={styles.errorHero}>
-              <Banner title="Project info not found">
-                <Link
-                  to="/"
-                  className="btn-white"
-                  style={{ textDecoration: "none" }}
-                >
-                  Return To Home
-                </Link>
-              </Banner>
-            </header>
-          </>
-        ) : (
-          <>
-            <header className={styles.projectHero}>
-              {/* <h2>{currentProject.title}</h2> */}
-              <Banner title={currentProject.title}></Banner>
-            </header>
-            <div className={styles.projectInfo}>
-              <div>
-                <h2>Images</h2>
-                {[currentProject.social_image, ...currentProject.images].map(
-                  (value, index) => (
-                    <div className={styles.imageContainer} key={index}>
-                      <div className={styles.imageHover}>
-                        <FaEye
-                          onClick={() => changeSelectedImage(value)}
-                        ></FaEye>
-                      </div>
-                      <img
-                        src={value}
-                        alt={currentProject.title}
-                        className={styles.projectImage}
-                      ></img>
-                    </div>
-                  )
-                )}
-              </div>
-              <div>
-                <h2>Project info</h2>
-                <p>{this.state.selectedImage}</p>
-              </div>
+      <>
+        {this.state.selectedImage && (
+          <div className={styles.modal}>
+            <div className={styles.modalImageContainer}>
+              <FaTimes onClick={() => changeSelectedImage()}></FaTimes>
+              <img
+                src={this.state.selectedImage}
+                alt={currentProject.title}
+              ></img>
             </div>
-          </>
+          </div>
         )}
-      </Layout>
+        <Layout>
+          {!currentProject ? (
+            <>
+              <header className={styles.errorHero}>
+                <Banner title="Project info not found">
+                  <Link
+                    to="/"
+                    className="btn-white"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Return To Home
+                  </Link>
+                </Banner>
+              </header>
+            </>
+          ) : (
+            <>
+              <header className={styles.projectHero}>
+                {/* <h2>{currentProject.title}</h2> */}
+                <Banner title={currentProject.title}></Banner>
+              </header>
+              <div className={styles.projectInfo}>
+                <div>
+                  <h2>Images</h2>
+                  {[currentProject.social_image, ...currentProject.images].map(
+                    (value, index) => (
+                      <div className={styles.imageContainer} key={index}>
+                        <div className={styles.imageHover}>
+                          <FaEye
+                            onClick={() => changeSelectedImage(value)}
+                          ></FaEye>
+                        </div>
+                        <img
+                          src={value}
+                          alt={currentProject.title}
+                          className={styles.projectImage}
+                        ></img>
+                      </div>
+                    )
+                  )}
+                </div>
+                <div>
+                  <h2>Project info</h2>
+                </div>
+              </div>
+            </>
+          )}
+        </Layout>
+      </>
     )
   }
 }
