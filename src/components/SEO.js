c6d033bfdd1c3c7c090ced9ccbf20218dbdc8160
oreste-abizera <oresteabizera11@gmail.com>
2021-04-17 21:49:52 +0200

    Twitter and Facebook cards info update

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -38,20 +38,17 @@ const SEO = ({ title, description, socialImage }) => {
       {/* facebook cards */}
       <meta property="og:url" content={siteUrl} />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={title || siteTitle} />
-      <meta property="og:description" content={description || siteDesc} />
-      <meta property="og:image" content={socialImage || `${siteUrl}${image}`} />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={siteDesc} />
+      <meta property="og:image" content={`${siteUrl}${image}`} />
       <meta property="og:image:width" content="400" />
       <meta property="og:image:height" content="300" />
       {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitterUsername} />
-      <meta name="twitter:title" content={title || siteTitle} />
-      <meta name="twitter:description" content={description || siteDesc} />
-      <meta
-        name="twitter:image"
-        content={socialImage || `${siteUrl}${image}`}
-      />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={siteDesc} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
