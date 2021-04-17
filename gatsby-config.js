0a036b24af0ad9300bf7d2fad72c13bf434595df
oreste-abizera <oresteabizera11@gmail.com>
2021-04-17 20:59:44 +0200

    Configuring filesystem and adding default background

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,18 @@ module.exports = {
       "I am a Software Engineer based in Rwanda. I focus on writing some codes everyday to solve many problems. I use React js, Next js and Gatsby for frontend on the web. Node js helps me on backend and React Native for mobile development.",
     author: "@abizeraoreste",
     keywords: ["Gatsby"],
+    twitterUsername: "@AbizeraOreste",
+    image: "/defaultBcg.png",
+    siteUrl: "http://oresteabizera.me",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
