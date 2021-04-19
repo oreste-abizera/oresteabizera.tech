c346181db2b43e9118517e7a287f3c46833002e8
oreste-abizera <oresteabizera11@gmail.com>
2021-04-19 20:46:11 +0200

    Adding sitemap and robots-txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,12 +19,21 @@ module.exports = {
     siteUrl: siteUrl,
   },
   plugins: [
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: siteUrl,
+        sitemap: `${siteUrl}/sitemap.xml`,
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
   ],
