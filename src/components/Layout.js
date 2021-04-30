058998bfd95d8246e3f1b5e478a1eca0c7bc8c6d
oreste-abizera <oresteabizera11@gmail.com>
2021-04-30 07:40:54 +0200

    top banner

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,11 +3,13 @@ import Footer from "./Footer"
 import Navbar from "./Navbar"
 import "../css/layout.css"
 import ScrollToTop from "./ScrollToTop"
+import TopBanner from "./TopBanner"
 
 export default class Layout extends Component {
   render() {
     return (
       <React.Fragment>
+        <TopBanner> </TopBanner>
         <ScrollToTop></ScrollToTop>
         <Navbar />
         <main className="main">{this.props.children}</main>
---
+++ src/components/TopBanner.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { FaTimes } from "react-icons/fa"
+
+export default function TopBanner() {
+  const [isShown, setIsShown] = React.useState(true)
+  const toggleIsShown = () => {
+    setIsShown(!isShown)
+  }
+  if (!isShown) {
+    return <></>
+  }
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "3rem",
+        backgroundColor: "#000",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        color: "#fff",
+        padding: "0.5rem",
+      }}
+    >
+      <div></div>
+      <p>
+        I released a new npm module,{" "}
+        <a
+          style={{ color: "#fff" }}
+          href="https://www.npmjs.com/package/string-method"
+          target="blank"
+        >
+          Check it out.
+        </a>
+      </p>
+      <FaTimes onClick={toggleIsShown} style={{ cursor: "pointer" }}></FaTimes>
+    </div>
+  )
+}
